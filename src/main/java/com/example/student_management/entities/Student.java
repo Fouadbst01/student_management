1f609b2cc2d109cbccfee0c1e12fc23fc12a18e4
Demon <demon@localhost.localdomain>
2022-04-11 12:49:34 +0200

    add JPA anotation to Student Entety

--- src/main/java/com/example/student_management/entities/Student.java
+++ src/main/java/com/example/student_management/entities/Student.java
@@ -4,15 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
 @Data
 @AllArgsConstructor @NoArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String firstName;
+    @Column(length = 50)
+    private String firstNamesutuatio;
+    @Column(length = 50)
     private String lastName;
+    @Column(length = 50,unique = true,nullable = false)
     private String Email;
     private Date birthDay;
-    private String Gender;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
     private boolean situation;
+
+    private enum Gender {
+        MALE,FEMALE
+    }
 }
